5eee9af172eac6606d427df9022112cb2fd55432
buildwithRaj-m <rajmaurya.tech@gmail.com>
2026-07-30 18:58:18 +0530

    Fixed bugs in PostCard and store.js

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,9 +8,10 @@ function PostCard({$id, title, featuredImage}) {
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
-
+                {featuredImage && (
+                    <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                    className='rounded-xl' />
+                )}
             </div>
             <h2
             className='text-xl font-bold'
